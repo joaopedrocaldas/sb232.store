7d0c213331a432ce8fb1be2b039f4056f30af2a1
joaopedrocaldas <jpecaldas@gmail.com>
2023-10-19 09:50:53 -0300

    pagina html e requisições http

--- main.js
+++ main.js
@@ -1,8 +1,58 @@
-$(function() {
+$(function () {
 
     const API = 'http://localhost:8080';
 
-    $('#product_create').click(function() {
+    Listar()
+
+    function Listar() {
+        $.ajax({
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            type: 'GET',
+            url: `${API}/product`,
+            success: function (result) {
+                console.log(result);
+
+                var html = "";
+                let i = 0;
+                result.forEach(element => {
+
+                    if (i % 3 === 0) {
+                        if (i !== 0) {
+                            html += '</div>';
+                        }
+                        html +=
+                            `
+                   <div class = "row">
+                   `
+                    }
+                    html +=
+                        `<div class="col-md-4">
+                            <div class="item">
+                                <h1>${element.name}</h1>
+                                <p>preço: ${element.preco}</p>
+                            </div>    
+                        </div>
+                        `
+                    i++
+                });
+
+                html += "</div>"
+                $("#listagem").html(html);
+
+            },
+            error(xhr, status, error) {
+                console.log(xhr);
+            }
+        })
+    }
+
+
+    $('#product_create').click(function () {
+
+
         var id = $('#product_id').val();
         var nome = $('#product_nome').val();
         var preco = $('#product_preco').val();
@@ -19,7 +69,7 @@ $(function() {
                 'nome': nome,
                 'preco': preco
             }),
-            success: function(response) {
+            success: function (response) {
                 console.log(response)
             }, error(xhr, status, error) {
                 console.log(xhr);
